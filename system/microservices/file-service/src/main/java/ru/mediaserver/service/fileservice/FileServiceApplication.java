36c7a2789ec5f9bfc4ee3c0f041d3838513e224d
vitalijbukatkin <vitaliy.bukatkin@gmail.com>
2019-06-18 09:41:17 +0300

    Docker compose

--- system/microservices/file-service/src/main/java/ru/mediaserver/service/fileservice/FileServiceApplication.java
+++ system/microservices/file-service/src/main/java/ru/mediaserver/service/fileservice/FileServiceApplication.java
@@ -3,18 +3,14 @@ package ru.mediaserver.service.fileservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import ru.mediaserver.service.fileservice.configuration.FileServiceConfiguration;
 
 @Configuration
 @SpringBootApplication
 public class FileServiceApplication {
-	private final FileServiceConfiguration fileServiceConfiguration;
 
-	public FileServiceApplication(FileServiceConfiguration fileServiceConfiguration) {
-		this.fileServiceConfiguration = fileServiceConfiguration;
-	}
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(1000 * 30);
 
-	public static void main(String[] args) {
 		SpringApplication.run(FileServiceApplication.class, args);
 	}
 }
